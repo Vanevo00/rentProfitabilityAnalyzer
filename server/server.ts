9855af2e9e4e7ab38f7292c64ff949304082f541
Vojtech Vanek <vanevo00@gmail.com>
2020-08-06 18:06:02 +0200

    feat: modularize schema

--- server/server.ts
+++ server/server.ts
@@ -1,59 +1,16 @@
 import express from 'express'
 import cors from 'cors'
 import connectDB from './config/db'
-import { ApolloServer, gql } from 'apollo-server-express'
-import { City } from './models'
+import { ApolloServer } from 'apollo-server-express'
+import schema from './schema'
 
-const typeDefs = gql`
-  type City {
-      id: ID!
-      name: String!
-      country: String!
-      mainImageLink: String
-      popularity: Int
-  }
-  
-  type Query {
-      getCities: [City]
-  }
-  
-  type Mutation {
-      addCity(
-          name: String!
-          country: String!
-          mainImageLink: String
-          popularity: Int
-      ): City
-  }
-`
-
-const resolvers = {
-  Query: {
-    getCities: async () => await City.find({}).exec()
-  },
-  Mutation: {
-    addCity: async (_, args: any) => {
-      try {
-        const popularity = args.popularity || 0
-        return await City.create({
-          ...args,
-          popularity
-        })
-      } catch (err) {
-        console.error(err)
-      }
-    }
-  }
-}
-
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({ schema })
 const app = express()
 server.applyMiddleware({ app })
 const port = 8999
 
 connectDB()
 
-// middleware
 app.use(express.json()) // to accept body data
 app.options('/', cors()) // CORS pre-flight
 app.use(cors()) // enable CORS
